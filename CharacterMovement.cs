947d91191005f7e4999fadeb90e0a61e2133a534
Patrick Oliveira Vagetti <77699722+Vagetti-dev@users.noreply.github.com>
2023-01-14 21:16:30 -0300

    Movimento em 3D
    Movimentação em 3D na Unity utilizando C#.
    
    Isso irá mover o personagem para a esquerda, direita, frente e trás quando as teclas A, D, W e S são pressionadas, respectivamente. Ele também permitirá que o personagem pule quando a tecla de espaço é pressionada.

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -5,6 +5,7 @@ public class CharacterMovement : MonoBehaviour
     public float moveSpeed = 10.0f;
     public float jumpForce = 10.0f;
     public float groundedDistance = 0.2f;
+    public float gravity = -9.81f;
     
     private Rigidbody rb;
     private bool isGrounded;
@@ -18,17 +19,25 @@ public class CharacterMovement : MonoBehaviour
     {
         // Check if the character is grounded
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundedDistance);
-        
-        // Get input for horizontal movement
+
+        // Get input for horizontal and vertical movement
         float moveX = Input.GetAxis("Horizontal");
-        // Apply horizontal movement
-        rb.AddForce(transform.right * moveX * moveSpeed, ForceMode.VelocityChange);
-        
+        float moveZ = Input.GetAxis("Vertical");
+
+        // Calculate the desired velocity
+        Vector3 desiredVelocity = new Vector3(moveX, 0, moveZ) * moveSpeed;
+
+        // Apply the desired velocity
+        rb.AddForce(desiredVelocity - rb.velocity, ForceMode.VelocityChange);
+
         // Check if the jump button is pressed
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             // Apply upward force for jumping
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+        // Apply gravity
+        rb.AddForce(Vector3.up * gravity, ForceMode.Acceleration);
     }
 }
