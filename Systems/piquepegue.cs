5df43cf81e9d7c16e19c643002abfcf67eab900f
Patrick Oliveira Vagetti <77699722+Vagetti-dev@users.noreply.github.com>
2023-02-18 08:02:27 -0300

    Create piquepegue.cs
    System that checks the collision between players on the mirror.

---
+++ Systems/piquepegue.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class PiquePegueSystem : NetworkBehaviour
+{
+    private string _playerTag = "Player";
+    public GameObject canvas;
+    private bool youAreTheCaught;
+
+
+    void Start(){
+        youAreTheCaught = false;
+    }
+
+    void Update(){
+        if(youAreTheCaught == true){
+            canvas.gameObject.SetActive(true);
+        } else {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider _col)
+    {
+        if (_col.gameObject.tag == _playerTag && !isLocalPlayer && youAreTheCaught == true)
+        {
+            youAreTheCaught = false;
+        }
+
+        if (_col.gameObject.tag == _playerTag && !isLocalPlayer && youAreTheCaught == false)
+        {
+            youAreTheCaught = true;
+        }
+    }
+}
